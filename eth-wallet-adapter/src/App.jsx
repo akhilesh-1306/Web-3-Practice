74bfa211ae11c7cd432e1a3cb1bf62c589af11f9
akhilesh-1306 <chaksu19@gmail.com>
2025-01-20 12:23:48 +0530

    send ETH functionality working

--- eth-wallet-adapter/src/App.jsx
+++ eth-wallet-adapter/src/App.jsx
@@ -3,6 +3,7 @@ import { mainnet } from 'wagmi/chains'
 import { injected } from 'wagmi/connectors'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { WagmiProvider } from 'wagmi'
+import { parseEther } from 'viem'
 // import { config } from './config'
 
 const projectId = '<WALLETCONNECT_PROJECT_ID>'
